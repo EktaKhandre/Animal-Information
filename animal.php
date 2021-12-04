414921d711dae5c3e2cf91d557aff3a68e43d1c0
EktaKhandre <64482053+EktaKhandre@users.noreply.github.com>
2021-12-04 21:52:30 +0530

    firstcopy

---
+++ animal.php
@@ -0,0 +1,162 @@
+<html>
+<head>
+  <title>Display Animal Information </title>
+</head>
+<?php
+
+session_start();
+
+// Create database connections
+$dbHost     = "sql307.epizy.com";
+$dbUsername = "epiz_30516472";
+$dbPassword = "gWXWJPDpJo0iK";
+$dbName     = "epiz_30516472_animal_database";
+
+$con = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+
+
+// Check connection
+if(!$con){
+  die("Connection failed: " .mysqli_connect_error());
+}
+
+?>
+<style>
+/* Styling the table */
+
+#tab{
+	
+	padding-left:70px;
+	
+	margin-top:0px;
+	margin-bottom: 90px;
+  margin-left: 250px;
+	border-collapse:collapse;
+	font-family:Cambria;
+	font-size: 17px;
+	
+}
+#tab th{
+padding:10px 10px 10px 10px;
+border: 2px solid black;
+}
+#tab td {
+padding:10px 10px 10px 10px;
+
+border: 2px solid black;
+}
+
+
+</style>
+
+<body style="background-color:#d0e9f1;">
+<h2 style="font-family:Cambria;"><left> ANIMAL INFORMATION </left></h2>
+
+<?php 
+      //Display view counter
+      if(isset($_SESSION['views']))
+           $_SESSION['views'] = $_SESSION['views']+1;
+      else
+           $_SESSION['views']=1;
+   
+?>
+<p style="font-family:Cambria; font-size: 17px;">View Counter: <?php echo $_SESSION['views']; ?></p>
+<form action="" method = "GET">
+  <!-- Filters of Category and Life Expectency -->
+  <table style="font-family:Cambria; font-size:20px; margin-top: 20px;" >
+    <tr>                                            
+    <td>
+    <select name="a_category" style="height:30px; width:400px; font-family:Cambria;" >
+    <option>Select Category</option>
+    <option value="Herbivores">Herbivores</option>
+    <option value="Carnivores">Carnivores</option>
+    <option value="Omnivores">Omnivores</option></select>
+    </td>
+    <td>
+    <select name="a_life" style="height:30px; width:400px; font-family:Cambria;" >
+    <option>Select Life Expectancy</option>
+    <option value="0-1">0 - 1 year</option>
+    <option value="1-5">1 - 5 years</option>
+    <option value="5-10">5 - 10 years</option>
+    <option value="10+">10 + years</option></select>
+    </td>
+
+    <td> <button type="submit">Search</button> </td>
+ 
+</tr>
+  </table>
+</form>
+<?php 
+    
+?>
+<!-- Display List of Animal Information -->
+<table  id ="tab" style="border: 2px solid ; font-family:Cambria; font-size:20px; margin-top: 20px; padding: 80px; background-color:#FFFAFA">
+<tr>
+     
+      <th>Name</th>
+      <th>Category</th>
+      <th>Image</th>
+      <th>Description</th>
+      <th>Life Expectancy</th>
+</tr>
+<?php 
+    
+    if(isset($_GET['a_category']) && isset($_GET['a_life']) )
+
+    {
+      $select1=$_GET['a_category'];   
+      $select2=$_GET['a_life']; 
+      $query1 =mysqli_query($con,"select * from animal where category = '$select1' and life = '$select2' ORDER BY date,name ASC ") ; // Filter data as per selected
+      if(mysqli_num_rows($query1)>0)
+      {
+         foreach($query1 as $row1){
+    
+?>
+<tr>
+     
+      <td><?php echo $row1['name']; ?></td>
+      <td><?php echo $row1['category']; ?></td>
+      <td><img src='<?php echo "upload/".$row1['image']; ?>'  width="230" height="200"></td>
+      <td><?php echo $row1['description']; ?></td>
+      <td><?php echo $row1['life']; ?></td>
+</tr>
+<?php     
+      }
+    }
+    else{
+      echo "No Record Found";
+    }
+  }
+  else{
+    $query2 =mysqli_query($con,"select * from animal ORDER BY name,date ASC") ; // Display data alphabetically
+    if(mysqli_num_rows($query2)>0)
+    {
+      foreach($query2 as $row2){
+    
+?>
+<tr>
+     
+      <td><?php echo $row2['name']; ?></td>
+      <td><?php echo $row2['category']; ?></td>
+      <td><img src='<?php echo "upload/".$row2['image']; ?>'  width="230" height="200"></td>
+      <td><?php echo $row2['description']; ?></td>
+      <td><?php echo $row2['life']; ?></td>
+</tr>
+<?php     
+      }
+    }
+    else{
+      echo "No Record Found for animals";
+    }
+  }
+
+ 
+?>
+
+</table>
+
+
+
+</div>
+</body>
+</html>
---
+++ submission.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Animal Information</title>
+
+<?php 
+
+session_start();
+
+// Create database connections
+$dbHost     = "sql307.epizy.com";
+$dbUsername = "epiz_30516472";
+$dbPassword = "gWXWJPDpJo0iK";
+$dbName     = "epiz_30516472_animal_database";
+$con = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+
+
+// Check connection
+if(!$con){
+  die("Connection failed: " .mysqli_connect_error());
+}
+
+
+?>
+
+<style>
+.buttonbg
+{
+	
+	border-radius:2px;
+	width:100px;
+	height:40px;
+	font-family:Cambria;
+	border-radius: 10px 10px 10px 10px;
+	font-size:20px;
+	
+}
+.buttonbg:hover
+{
+	background-color:#ADD8E6;
+	color:#000000;
+}
+
+.f1{
+	padding: 50px 20px 50px 400px;
+	background-color:#d0e9f1;
+}
+
+<?php 
+// Declare varaibles for captcha
+	$randomno1=mt_rand(1,9);
+	$randomno2=mt_rand(1,9);
+?>
+
+</style>
+</head>
+<body>
+	<!-- Input form for animal information -->
+<div class='f1'>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" enctype="multipart/form-data">   
+	<h2 style="font-family:Cambria;"><left> ANIMAL INFORMATION </left></h2>
+		            
+	<table style="border: 2px solid ; font-family:Cambria; font-size:20px; margin-top: 20px; padding: 80px; background-color:#FFFAFA">
+				
+		<tr>
+			<td style="padding: 10px 20px 15px 20px;"><b> NAME:</b></td>
+		    <td style="padding: 10px 20px 15px 20px;"><input type="text" name="a_name" placeholder="" style="height:30px; width:400px; font-family:Cambria;" required/>					</td>
+		</tr>
+		<tr>
+			<td style="padding: 10px 20px 15px 20px;"><b> CATEGORY :</b></td>
+			<td style="padding: 10px 20px 15px 20px;"><select name="a_category" style="height:30px; width:400px; font-family:Cambria;" required>
+													    <option value="Herbivores">Herbivores</option>
+                                                        <option value="Carnivores">Carnivores</option>
+                                                        <option value="Omnivores">Omnivores</option>
+			</select>	
+		    </td>
+		</tr>
+        <tr>
+			<td style="padding: 10px 20px 15px 20px;"><b> IMAGE :</b></td>
+			<td style="padding: 10px 20px 15px 20px;"><input type="file" name="a_image" style="height:30px; width:400px; font-family:Cambria;" required></td>
+		</tr>
+		<tr>
+			<td style="padding: 10px 20px 15px 20px;"><b> DESCRIPTION :</b></td>
+			<td style="padding: 10px 20px 15px 20px;"><textarea name="a_description" placeholder="" style="height:50px; width:400px; font-family:Cambria;" required></textarea></td>
+		</tr>
+		<tr>
+			<td style="padding: 10px 20px 15px 20px;"><b> LIFE EXPECTANCY :</b></td>
+			<td style="padding: 10px 20px 15px 20px;"><select name="a_life" style="height:40px; width:400px; font-family:Cambria;" required>
+																  <option value="0-1">0 - 1 year</option>
+                                                                  <option value="1-5">1 - 5 years</option>
+                                                                  <option value="5-10">5 - 10 years</option>
+                                                                  <option value="10+">10 + years</option>
+													   </select>	
+			</td>
+		</tr>			
+		<tr>			
+			<td style="padding: 10px 20px 15px 180px;"><?php echo $randomno1 . ' + ' . $randomno2 . ' = ' ?></td>
+			<td style="padding: 10px 10px 15px 20px;"> <input type="number" name="capResult"></td>
+            <input type="hidden" name="random1" value="<?php echo $randomno1 ?>" >
+			<input type="hidden" name="random2" value="<?php echo $randomno2 ?>" >
+			
+		<tr>
+			<td height="49" colspan="" style="padding-left:150px ; padding-top:10px;">
+			 <input type="reset" name="btnClear" value="Clear" class="buttonbg"/></td>
+			<td height="49" colspan="2" style="padding-left: 160px; padding-top:10px;">
+			 <input type="submit" name="btnSave" value="Save" class="buttonbg"/>  </td>
+					    
+		</tr>		
+		<div>
+			
+	</table>
+</form> 
+</div>
+</body>
+<?php
+
+    $statusMsg = '';
+	
+	$checkcap= $randomno1+$randomno2;
+	// Saves data into database
+	if(isset($_POST['btnSave'])){
+        
+        $r1=$_POST['random1'];
+		$r2=$_POST['random2'];
+		$checkcap= $r1+$r2;
+		$res=$_POST['capResult'];
+	    if(  $res == $checkcap)      //comparing captcha
+		{
+			$name= $_POST['a_name'];
+			$category= $_POST['a_category'];
+			$image= $_FILES['a_image']['name'];
+			$discription= $_POST['a_description'];
+			$life= $_POST['a_life'];
+		
+			$query="insert into animal(name,category,image,description,life,date)values('$name','$category','$image','$discription','$life',CURDATE())";
+			$res=mysqli_query($con, $query);
+			if($res)
+			{
+				move_uploaded_file($_FILES["a_image"]["tmp_name"], "upload/".$_FILES["a_image"]["name"]);
+				  $statusMsg = "Record Added";
+			}
+			else{
+				 $statusMsg = "Something went wrong, Please try again";
+			}
+			
+		
+		
+	// Display status message
+	echo "<script type='text/javascript'>alert('{$statusMsg}');document.location='animal.php';</script>";
+		}
+		else
+		{
+			echo '<script>alert("You entered an incorrect Captcha")</script>' ;
+		}
+
+		
+	}
+?>
