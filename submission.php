ecb3c10d16d2adc2b8c60c03fc7417eee2d039f0
EktaKhandre <64482053+EktaKhandre@users.noreply.github.com>
2021-12-05 10:14:15 +0530

    secondcopy

--- submission.php
+++ submission.php
@@ -2,11 +2,8 @@
 <html lang="en">
 <head>
 	<title>Animal Information</title>
-
 <?php 
-
 session_start();
-
 // Create database connections
 $dbHost     = "sql307.epizy.com";
 $dbUsername = "epiz_30516472";
@@ -14,13 +11,10 @@ $dbPassword = "gWXWJPDpJo0iK";
 $dbName     = "epiz_30516472_animal_database";
 $con = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
 
-
 // Check connection
 if(!$con){
   die("Connection failed: " .mysqli_connect_error());
 }
-
-
 ?>
 
 <style>
@@ -57,25 +51,23 @@ if(!$con){
 <body>
 	<!-- Input form for animal information -->
 <div class='f1'>
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" enctype="multipart/form-data">   
-	<h2 style="font-family:Cambria;"><left> ANIMAL INFORMATION </left></h2>
-		            
-	<table style="border: 2px solid ; font-family:Cambria; font-size:20px; margin-top: 20px; padding: 80px; background-color:#FFFAFA">
-				
+	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" enctype="multipart/form-data">   
+		<h2 style="font-family:Cambria;"><left> ANIMAL INFORMATION </left></h2>
+		<table style="border: 2px solid ; font-family:Cambria; font-size:20px; margin-top: 20px; padding: 80px; background-color:#FFFAFA">
 		<tr>
-			<td style="padding: 10px 20px 15px 20px;"><b> NAME:</b></td>
-		    <td style="padding: 10px 20px 15px 20px;"><input type="text" name="a_name" placeholder="" style="height:30px; width:400px; font-family:Cambria;" required/>					</td>
+		 	<td style="padding: 10px 20px 15px 20px;"><b> NAME:</b></td>
+		      	<td style="padding: 10px 20px 15px 20px;"><input type="text" name="a_name" placeholder="" style="height:30px; width:400px; font-family:Cambria;" required/>					</td>
 		</tr>
 		<tr>
 			<td style="padding: 10px 20px 15px 20px;"><b> CATEGORY :</b></td>
 			<td style="padding: 10px 20px 15px 20px;"><select name="a_category" style="height:30px; width:400px; font-family:Cambria;" required>
-													    <option value="Herbivores">Herbivores</option>
+							<option value="Herbivores">Herbivores</option>
                                                         <option value="Carnivores">Carnivores</option>
                                                         <option value="Omnivores">Omnivores</option>
-			</select>	
-		    </td>
+			                                </select>	
+		        </td>
 		</tr>
-        <tr>
+        	<tr>
 			<td style="padding: 10px 20px 15px 20px;"><b> IMAGE :</b></td>
 			<td style="padding: 10px 20px 15px 20px;"><input type="file" name="a_image" style="height:30px; width:400px; font-family:Cambria;" required></td>
 		</tr>
@@ -86,41 +78,36 @@ if(!$con){
 		<tr>
 			<td style="padding: 10px 20px 15px 20px;"><b> LIFE EXPECTANCY :</b></td>
 			<td style="padding: 10px 20px 15px 20px;"><select name="a_life" style="height:40px; width:400px; font-family:Cambria;" required>
-																  <option value="0-1">0 - 1 year</option>
+								  <option value="0-1">0 - 1 year</option>
                                                                   <option value="1-5">1 - 5 years</option>
                                                                   <option value="5-10">5 - 10 years</option>
                                                                   <option value="10+">10 + years</option>
-													   </select>	
+				                                  </select>	
 			</td>
 		</tr>			
 		<tr>			
 			<td style="padding: 10px 20px 15px 180px;"><?php echo $randomno1 . ' + ' . $randomno2 . ' = ' ?></td>
 			<td style="padding: 10px 10px 15px 20px;"> <input type="number" name="capResult"></td>
-            <input type="hidden" name="random1" value="<?php echo $randomno1 ?>" >
+                        <input type="hidden" name="random1" value="<?php echo $randomno1 ?>" >
 			<input type="hidden" name="random2" value="<?php echo $randomno2 ?>" >
 			
 		<tr>
 			<td height="49" colspan="" style="padding-left:150px ; padding-top:10px;">
-			 <input type="reset" name="btnClear" value="Clear" class="buttonbg"/></td>
+			<input type="reset" name="btnClear" value="Clear" class="buttonbg"/></td>
 			<td height="49" colspan="2" style="padding-left: 160px; padding-top:10px;">
-			 <input type="submit" name="btnSave" value="Save" class="buttonbg"/>  </td>
-					    
+			<input type="submit" name="btnSave" value="Save" class="buttonbg"/>  </td>
 		</tr>		
-		<div>
-			
+		<div>	
 	</table>
 </form> 
 </div>
 </body>
 <?php
-
     $statusMsg = '';
-	
-	$checkcap= $randomno1+$randomno2;
-	// Saves data into database
+    $checkcap= $randomno1+$randomno2;
+    // Saves data into databas
 	if(isset($_POST['btnSave'])){
-        
-        $r1=$_POST['random1'];
+        	$r1=$_POST['random1'];
 		$r2=$_POST['random2'];
 		$checkcap= $r1+$r2;
 		$res=$_POST['capResult'];
@@ -142,17 +129,13 @@ if(!$con){
 			else{
 				 $statusMsg = "Something went wrong, Please try again";
 			}
-			
-		
 		
 	// Display status message
-	echo "<script type='text/javascript'>alert('{$statusMsg}');document.location='animal.php';</script>";
+	   echo "<script type='text/javascript'>alert('{$statusMsg}');document.location='animal.php';</script>";
 		}
 		else
 		{
 			echo '<script>alert("You entered an incorrect Captcha")</script>' ;
 		}
-
-		
 	}
 ?>
