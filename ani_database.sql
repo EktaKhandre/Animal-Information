e8743c5bdb75d1c3c78a00b839d9d74c1bf47863
EktaKhandre <64482053+EktaKhandre@users.noreply.github.com>
2021-12-06 18:51:49 +0530

    secondcopy

--- ani_database.sql
+++ ani_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 06, 2021 at 12:55 PM
+-- Generation Time: Dec 06, 2021 at 01:19 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `animal`;
 CREATE TABLE IF NOT EXISTS `animal` (
-  `id` int(255) NOT NULL,
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `category` varchar(255) NOT NULL,
   `image` blob NOT NULL,
@@ -37,16 +37,14 @@ CREATE TABLE IF NOT EXISTS `animal` (
   `life` varchar(255) NOT NULL,
   `Date` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `animal`
 --
 
 INSERT INTO `animal` (`id`, `name`, `category`, `image`, `description`, `life`, `Date`) VALUES
-(14, 'Camel', 'Herbivores', 0x63616d656c2e4a5047, 'A camel is an even-toed ungulate in the genus Camelus that bears distinctive fatty deposits known as \"humps\" on its back. Camels have long been domesticated and, as livestock, they provide food (milk and meat) and textiles (fiber and felt from hair).', '10+', '2021-12-06'),
-(15, 'Tiger', 'Carnivores', 0x74696765722e4a5047, 'The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white underside.', '10+', '2021-12-06'),
-(16, 'Squirrel', 'Omnivores', 0x73712e4a5047, 'Squirrels are members of the family Sciuridae, a family that includes small or medium-size rodents. The squirrel family includes tree squirrels, ground squirrels, chipmunks, marmots (including groundhogs), flying squirrels, and prairie dogs amongst other rodents.', '5-10', '2021-12-06');
+(18, 'Tiger', 'Carnivores', 0x74696765722e4a5047, 'The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white underside.', '10+', '2021-12-06');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
